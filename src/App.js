f015337ef5fe6fc82219e2ba8aedca737ddcfecf
Callum <callum.taylor.reid@gmail.com>
2021-08-24 13:15:20 -0700

    Created useForm custom hook for form values and handleChange

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 
-import {useState} from 'react';
 
+import { useForm } from './useForm';
 
 const App = () => {
-  const [count, setCount] = useState(10);
+  const [values, handleChange] = useForm({email: '', password: ''});
+
   return (
     <div className="App">
-      <div>
-        <button onClick={() => setCount(currentCount => currentCount + 1)} >+</button>
-        <div>{count}</div>
-      </div>
+      <input name='email'
+      value={values.email}
+      onChange={handleChange}
+      ></input>
+      <input type='password'
+      name='password'
+      value={values.password}
+      onChange={handleChange}
+      ></input>
     </div>
   );
 }
